e30fa7261e03b3778f08ff58ba92b28178b55dc7
Mr147 <zhu_khua@163.com>
2019-02-14 17:39:36 +0800

    Update boc.py

--- boc.py
+++ boc.py
@@ -1,23 +1,29 @@
 import requests
-from bs4 import BeautifulSoup
 import time
+import random
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
 
-data_list = "["     #结果集json串
-baseUrl = 'http://www.bankofchina.com/sourcedb/operations%sindex%s.htm'     #获取数据通用url格式
-proAndCity = []     #省市码值数组
+data_list = "["
+baseUrl = 'http://www.bankofchina.com/sourcedb/operations%sindex%s.htm'
+proAndCity = []
+#代理
+proxiesIps = []
+#随机请求头
+ua = UserAgent(verify_ssl=False)
 
-#获取每页的数据
-def getData(add, totalPage):
-    global data_list
-    for page in range(int(totalPage)):
+def getData(add, totalPage, proxiesIp):
+    for page in range(totalPage):
         url = ''
+        headers={"User-Agent":ua.random}
+        proxies=proxiesIps[random.randint(0, len(proxiesIps)-1)]
+        print('ip:'+proxies)
         time.sleep(1)
         if page > 0:
             url = baseUrl %(add, "_"+str(page))
         else:
             url = baseUrl %(add, "")
-        print(url)
-        req = requests.get(url, {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36', 'timeout':'2'})
+        req = requests.get(url,headers=headers,proxies=proxies,timeout=10)
         req.encoding = 'utf-8'
         soup = BeautifulSoup(req.text, 'html.parser')
         tables = soup.findAll('table')
@@ -34,26 +40,38 @@ def getData(add, totalPage):
                 else:
                     data_list += "{\"bankName\":\"" + name + "\",\"city\":\"\",\"area\":\"" + address[0] + "\",\"postCode\":\"" + postCode[-7:-1] + "\"},\n"
 
-#获取该省市下的总页数
 def getTotalPage(add):
     url = baseUrl %(add, "")
-    req = requests.get(url, {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'})
+    req = requests.get(url, {'User-Agent':ua.random})
     req.encoding = 'utf-8'
     soup = BeautifulSoup(req.text, 'html.parser')
     return soup.find(class_='turn_page').p.span.string
 
-#获取省市的码值
 def getProAndCity():
     url = "http://www.bankofchina.com/sourcedb/operations/"
-    req = requests.get(url, {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'})
+    req = requests.get(url, {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'},timeout=10)
     req.encoding = 'utf-8'
     soup = BeautifulSoup(req.text, 'html.parser')
     aS = soup.find_all(class_='hui12_20_hover')
     for a in aS:
         proAndCity.append(a['href'][1:])
 
+def getProxies():
+    url = "https://www.xicidaili.com/wt/1"
+    req = requests.get(url, {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36'},timeout=10)
+    req.encoding = 'utf-8'
+    soup = BeautifulSoup(req.text, 'html.parser')
+    ips = soup.find_all('tr')
+    for tr in ips:
+        tds = tr.findAll('td')
+        proxiesIps.append({'http': tds[1].string+':'+tds[2]})
+    
+    
 getProAndCity()
 print(proAndCity)
+print('-----------------------------------------------')
+getProxies()
+print(proxiesIps)
 for add in proAndCity:
     print(add)
     totalPage = getTotalPage(add)
